ac47085dfa6cb78daa025ee0648b9b8cd268a17a
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-13 14:13:44 +0530

    modified the setup application for more better request

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ load_dotenv()
 
 app = Flask(__name__)
 
+import os
+
+RAPIDAPI_HOST = os.environ.get("RAPIDAPI_HOST")
+RAPIDAPI_KEY = os.environ.get("RAPIDAPI_KEY")
+RAPIDAPI_URL = os.environ.get("RAPIDAPI_URL")
+
+if not RAPIDAPI_HOST or not RAPIDAPI_KEY:
+    raise ValueError("RAPIDAPI_HOST or RAPIDAPI_KEY is not set")
 
 
 @app.route("/get_timeline", methods=["GET"])
@@ -17,13 +25,13 @@ def get_timeline():
 
 
     headers = {
-        "x-rapidapi-host": os.getenv(RAPIDAPI_HOST),
-        "x-rapidapi-key": os.getenv(RAPIDAPI_KEY),
+        "x-rapidapi-host": RAPIDAPI_HOST,
+        "x-rapidapi-key": RAPIDAPI_KEY,
     }
     params = {"screenname": screenname}
 
     try:
-        response = requests.get(os.getenv(RAPIDAPI_URL), headers=headers, params=params)
+        response = requests.get(RAPIDAPI_URL, headers=headers, params=params)
         response.raise_for_status()
         return jsonify(response.json())
     except requests.exceptions.RequestException as e:
